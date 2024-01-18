cc7e182a46b92032fc3db0ed2c2b4f201d744a6f
LianDev17 <liandevapp@gmail.com>
2024-01-18 21:55:00 +0100

    Init commit

---
+++ src/test/kotlin/ktorserver/liandev/ApplicationTest.kt
@@ -0,0 +1,21 @@
+package ktorserver.liandev
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+import ktorserver.liandev.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
